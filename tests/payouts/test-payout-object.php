19d905cdce00b369e33adabaa568929d336ddbdf
Drew Jaynes <drew@affiliatewp.com>
2016-08-03 14:47:32 -0600

    Stub other payouts test files.

---
+++ tests/payouts/test-payout-object.php
@@ -0,0 +1,18 @@
+<?php
+use AffWP\Affiliate\Payout;
+
+/**
+ * Tests for AffWP\Affiliate\Payout
+ *
+ * @covers AffWP\Affiliate\Payout
+ * @covers AffWP\Object
+ *
+ * @group payouts
+ * @group objects
+ */
+class AffWP_Affiliate_Payout_Tests extends WP_UnitTestCase {
+
+	public function test_payout_object() {
+		$this->assertTrue( true );
+	}
+}
---
+++ tests/payouts/test-payouts-db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Tests for Affiliate_WP_Payouts_DB class
+ *
+ * @covers Affiliate_WP_Payouts_DB
+ * @group database
+ * @group payouts
+ */
+class Payouts_DB_Tests extends WP_UnitTestCase {
+
+	public function test_payouts_db() {
+		$this->assertTrue( true );
+	}
+}
