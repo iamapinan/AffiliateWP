3cd9c062e087d6a83a86d9123fa4c365ad66c66e
Drew Jaynes <drew@affiliatewp.com>
2016-08-04 10:47:29 -0600

    Payouts actions stub.

---
+++ includes/admin/payouts/actions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 'Actions' logic for Payouts
+ *
+ * @package    AffiliateWP\Admin\Payouts
+ * @copyright  Copyright (c) 2014, Pippin Williamson
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since      1.9
+ */
