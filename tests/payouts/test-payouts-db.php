22751f05d95295648c41516c823b418ae4a49456
Drew Jaynes <drew@affiliatewp.com>
2016-08-05 23:40:39 -0600

    Add some tests for add() DB method.

--- tests/payouts/test-payouts-db.php
+++ tests/payouts/test-payouts-db.php
@@ -8,7 +8,75 @@
  */
 class Payouts_DB_Tests extends WP_UnitTestCase {
 
-	public function test_payouts_db() {
-		$this->assertTrue( true );
+
+	protected $_payout_id, $_affiliate_id, $_referral_id;
+
+	/**
+	 * Set up.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->_affiliate_id = affiliate_wp()->affiliates->add( array(
+			'user_id' => $this->factory->user->create()
+		) );
+
+		$this->_referral_id = affiliate_wp()->referrals->add( array(
+			'affiliate_id' => $this->_affiliate_id
+		) );
+
+		$this->_payout_id = affiliate_wp()->affiliates->payouts->add( array(
+			'affiliate_id' => $this->_affiliate_id,
+			'referrals'    => $this->_referral_id,
+			'amount'       => '10.00'
+		) );
 	}
+
+	/**
+	 * Tear down.
+	 */
+	public function tearDown() {
+		affwp_delete_affiliate( $this->_affiliate_id );
+		affwp_delete_referral( $this->_referral_id );
+
+		parent::tearDown();
+	}
+
+	/**
+	 * @covers Affiliate_WP_Payouts_DB::add()
+	 */
+	public function test_add_should_return_false_if_affiliate_id_undefined() {
+		$this->assertFalse( affiliate_wp()->affiliates->payouts->add() );
+	}
+
+	/**
+	 * @covers Affiliate_WP_Payouts_DB::add()
+	 */
+	public function test_add_should_return_false_if_invalid_affiliate_id() {
+		$this->assertFalse( affiliate_wp()->affiliates->payouts->add( array(
+			'affiliate_id' => rand( 500, 5000 )
+		) ) );
+	}
+
+	/**
+	 * @covers Affiliate_WP_Payouts_DB::add()
+	 */
+	public function test_add_should_return_false_if_no_referrals_defined() {
+		$this->assertFalse( affiliate_wp()->affiliates->payouts->add( array(
+			'affiliate_id' => $this->_affiliate_id
+		) ) );
+	}
+
+	/**
+	 * @covers Affiliate_WP_Payouts_DB::add()
+	 */
+	public function test_add_should_convert_array_of_referral_ids_to_comma_separated_string() {
+		$payout_id = affiliate_wp()->affiliates->payouts->add( array(
+			'affiliate_id' => $this->_affiliate_id,
+			'referrals'    => range( 1, 3 )
+		) );
+
+		$this->assertSame( '1,2,3', affiliate_wp()->affiliates->payouts->get_column( 'referrals', $payout_id ) );
+	}
+
 }
