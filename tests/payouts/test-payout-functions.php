dcf2886df1e66557cca36c9268a3669bb98abf58
Drew Jaynes <drew@affiliatewp.com>
2016-08-05 23:27:32 -0600

    Add tests for affwp_get_payout_referrals() and affwp_get_payout_status_label().

--- tests/payouts/test-payout-functions.php
+++ tests/payouts/test-payout-functions.php
@@ -70,4 +70,74 @@ class Payout_Function_Tests extends WP_UnitTestCase {
 		$this->assertInstanceOf( 'AffWP\Affiliate\Payout', affwp_get_payout( $payout ) );
 	}
 
+	/**
+	 * @covers affwp_get_payout_referrals()
+	 */
+	public function test_get_payout_referrals_should_return_false_if_invalid_payout() {
+		$this->assertFalse( affwp_get_payout_referrals( 0 ) );
+		$this->assertFalse( affwp_get_payout_referrals( new \stdClass() ) );
+	}
+
+	/**
+	 * @covers affwp_get_payout_referrals()
+	 */
+	public function test_get_payout_referrals_should_return_array_of_referral_objects() {
+		$referrals = array();
+		for ( $i = 1; $i <= 3; $i++ ) {
+			$referrals[] = affiliate_wp()->referrals->add( array(
+				'affiliate_id' => $this->_affiliate_id
+			) );
+		}
+
+		$payout_id = affiliate_wp()->affiliates->payouts->add( array(
+			'affiliate_id' => $this->_affiliate_id,
+			'referrals'    => $referrals
+		) );
+
+		$payout_referrals = affwp_get_payout_referrals( $payout_id );
+
+		$this->assertSame( $referrals, wp_list_pluck( $payout_referrals, 'referral_id' ) );
+		$this->assertInstanceOf( 'AffWP\Referral', $payout_referrals[0] );
+	}
+
+	/**
+	 * @covers affwp_get_payout_status_label()
+	 */
+	public function test_get_payout_status_label_should_return_false_if_invalid_payout() {
+		$this->assertFalse( affwp_get_payout_status_label( 0 ) );
+		$this->assertFalse( affwp_get_payout_status_label( new \stdClass() ) );
+	}
+
+	/**
+	 * @covers affwp_get_payout_status_label()
+	 */
+	public function test_get_payout_status_label_should_return_paid_status_by_default() {
+		$this->assertSame( 'Paid', affwp_get_payout_status_label( $this->_payout_id ) );
+	}
+
+	/**
+	 * @covers affwp_get_payout_status_label()
+	 */
+	public function test_get_payout_status_label_should_return_payout_status_label() {
+		$payout_id = affiliate_wp()->affiliates->payouts->add( array(
+			'affiliate_id' => $this->_affiliate_id,
+			'status'       => 'failed',
+			'referrals'    => range( 100, 103 )
+		) );
+
+		$this->assertSame( 'Failed', affwp_get_payout_status_label( $payout_id ) );
+	}
+
+	/**
+	 * @covers affwp_get_payout_status_label()
+	 */
+	public function test_get_payout_status_label_should_return_paid_if_invalid_status() {
+		$payout_id = affiliate_wp()->affiliates->payouts->add( array(
+			'affiliate_id' => $this->_affiliate_id,
+			'status'       => 'foo',
+			'referrals'    => range( 50, 55 )
+		) );
+
+		$this->assertSame( 'Paid', affwp_get_payout_status_label( $payout_id ) );
+	}
 }
