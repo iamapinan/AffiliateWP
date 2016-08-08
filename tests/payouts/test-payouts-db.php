ff1c82fa472a22ae99b2a0165cf4dceb898020ff
Drew Jaynes <drew@affiliatewp.com>
2016-08-08 13:45:33 -0600

    Convert Payouts DB tests to use the payout factory.

--- tests/payouts/test-payouts-db.php
+++ tests/payouts/test-payouts-db.php
@@ -6,50 +6,7 @@
  * @group database
  * @group payouts
  */
-class Payouts_DB_Tests extends WP_UnitTestCase {
-
-
-	protected $_affiliate_id, $_affiliate_id2, $_referral_id;
-
-	protected $_payouts = array();
-	protected $_referrals = array();
-
-	/**
-	 * Set up.
-	 */
-	public function setUp() {
-		parent::setUp();
-
-		$this->_referrals = range( 30, 33 );
-
-		$this->_affiliate_id = affiliate_wp()->affiliates->add( array(
-			'user_id' => $this->factory->user->create()
-		) );
-
-		$this->_affiliate_id2 = affiliate_wp()->affiliates->add( array(
-			'user_id' => $this->factory->user->create()
-		) );
-
-		$this->_referral_id = affiliate_wp()->referrals->add( array(
-			'affiliate_id' => $this->_affiliate_id
-		) );
-	}
-
-	/**
-	 * Tear down.
-	 */
-	public function tearDown() {
-		affwp_delete_affiliate( $this->_affiliate_id );
-		affwp_delete_affiliate( $this->_affiliate_id2 );
-
-		affwp_delete_referral( $this->_referral_id );
-
-		foreach ( $this->_payouts as $payout_id ) {
-			affwp_delete_payout( $payout_id );
-		}
-
-		parent::tearDown();
-	}
+class Payouts_DB_Tests extends AffiliateWP_UnitTestCase {
 
 	/**
 	 * @covers Affiliate_WP_Payouts_DB::add()
@@ -71,8 +28,10 @@ class Payouts_DB_Tests extends WP_UnitTestCase {
 	 * @covers Affiliate_WP_Payouts_DB::add()
 	 */
 	public function test_add_should_return_false_if_no_referrals_defined() {
+		$affiliate_id = $this->affwp->affiliate->create();
+
 		$this->assertFalse( affiliate_wp()->affiliates->payouts->add( array(
-			'affiliate_id' => $this->_affiliate_id
+			'affiliate_id' => $affiliate_id
 		) ) );
 	}
 
@@ -80,9 +39,12 @@ class Payouts_DB_Tests extends WP_UnitTestCase {
 	 * @covers Affiliate_WP_Payouts_DB::add()
 	 */
 	public function test_add_should_convert_array_of_referral_ids_to_comma_separated_string() {
-		$this->_set_up_payouts( 1 );
+		$payout_id = $this->affwp->payout->create();
 
-		$this->assertSame( '30,31,32,33', affiliate_wp()->affiliates->payouts->get_column( 'referrals', $this->_payouts[0] ) );
+		$referrals = affiliate_wp()->affiliates->payouts->get_referral_ids( $payout_id );
+		$referrals = implode( ',', $referrals );
+
+		$this->assertSame( $referrals, affiliate_wp()->affiliates->payouts->get_column( 'referrals', $payout_id ) );
 	}
 
 	/**
@@ -96,9 +58,9 @@ class Payouts_DB_Tests extends WP_UnitTestCase {
 	 * @covers Affiliate_WP_Payouts_DB::payout_exists()
 	 */
 	public function test_payout_exists_should_return_true_if_payout_exists() {
-		$this->_set_up_payouts( 1 );
+		$payout_id = $this->affwp->payout->create();
 
-		$this->assertTrue( affiliate_wp()->affiliates->payouts->payout_exists( $this->_payouts[0] ) );
+		$this->assertTrue( affiliate_wp()->affiliates->payouts->payout_exists( $payout_id ) );
 	}
 
 	/**
@@ -158,9 +120,9 @@ class Payouts_DB_Tests extends WP_UnitTestCase {
 	 * @covers Affiliate_WP_Payouts_DB::get_object()
 	 */
 	public function test_get_object_should_return_payout_object_if_valid_payout_id() {
-		$this->_set_up_payouts();
+		$payout_id = $this->affwp->payout->create();
 
-		$this->assertInstanceOf( 'AffWP\Affiliate\Payout', affiliate_wp()->affiliates->payouts->get_object( $this->_payouts[0] ) );
+		$this->assertInstanceOf( 'AffWP\Affiliate\Payout', affiliate_wp()->affiliates->payouts->get_object( $payout_id ) );
 	}
 
 	/**
@@ -181,15 +143,18 @@ class Payouts_DB_Tests extends WP_UnitTestCase {
 	 * @covers Affiliate_WP_Payouts_DB::get_referral_ids()
 	 */
 	public function test_get_referral_ids_should_return_an_array_of_referral_ids() {
-		$this->_set_up_payouts();
-		$this->assertEqualSets( $this->_referrals, affiliate_wp()->affiliates->payouts->get_referral_ids( $this->_payouts[0] ) );
+		$payout_id = $this->affwp->payout->create( array(
+			'referrals' => $referrals = $this->affwp->referral->create_many( 3 )
+		) );
+
+		$this->assertEqualSets( $referrals, affiliate_wp()->affiliates->payouts->get_referral_ids( $payout_id ) );
 	}
 
 	/**
 	 * @covers Affiliate_WP_Payouts_DB::get_payouts()
 	 */
 	public function test_get_payouts_number_should_return_number_if_available() {
-		$this->_set_up_payouts( 5 );
+		$this->affwp->payout->create_many( 5 );
 
 		$payouts = affiliate_wp()->affiliates->payouts->get_payouts( array(
 			'number' => 3
@@ -203,7 +168,7 @@ class Payouts_DB_Tests extends WP_UnitTestCase {
 	 * @covers Affiliate_WP_Payouts_DB::get_payouts()
 	 */
 	public function test_get_payouts_offset_should_offset_number_given() {
-		$this->_set_up_payouts( 5 );
+		$this->affwp->payout->create_many( 5 );
 
 		$all_payouts = affiliate_wp()->affiliates->payouts->get_payouts();
 
@@ -220,65 +185,43 @@ class Payouts_DB_Tests extends WP_UnitTestCase {
 	 */
 	public function test_get_payouts_with_single_affiliate_id_should_return_payouts_for_that_affiliate_only() {
 		// Total of 5 payouts, two different affiliates.
-		$this->_set_up_payouts();
+		$this->affwp->payout->create_many( 3 );
 
-		$this->_set_up_payouts( 2, array(
-			'affiliate_id' => $this->_affiliate_id2
+		$this->affwp->payout->create_many( 2, array(
+			'affiliate_id' => $affiliate_id = $this->affwp->affiliate->create()
 		) );
 
 		$payouts = affiliate_wp()->affiliates->payouts->get_payouts( array(
-			'affiliate_id' => $this->_affiliate_id2
+			'affiliate_id' => $affiliate_id
 		) );
 
 		$this->assertSame( 2, count( $payouts ) );
-		$this->assertSame( array( $this->_affiliate_id2 ), array_unique( wp_list_pluck( $payouts, 'affiliate_id' ) ) );
+		$this->assertSame( array( $affiliate_id ), array_unique( wp_list_pluck( $payouts, 'affiliate_id' ) ) );
 	}
 
 	/**
 	 * @covers Affiliate_WP_Payouts_DB::get_payouts()
 	 */
 	public function test_get_payouts_with_multiple_affiliate_ids_should_return_payouts_for_multiple_assuming_number() {
-		$this->_set_up_payouts( 2, array(
-			'affiliate_id' => $this->_affiliate_id
+		// Total of 4 payouts, two different affiliates.
+		$this->affwp->payout->create_many( 2, array(
+			'affiliate_id' => $affiliate1 = $this->affwp->affiliate->create()
 		) );
 
-		$this->_set_up_payouts( 2, array(
-			'affiliate_id' => $this->_affiliate_id2
+		$this->affwp->payout->create_many( 2, array(
+			'affiliate_id' => $affiliate2 = $this->affwp->affiliate->create()
 		) );
 
 		$payouts = affiliate_wp()->affiliates->payouts->get_payouts( array(
-			'affiliate_id' => array( $this->_affiliate_id, $this->_affiliate_id2 )
+			'affiliate_id' => array( $affiliate1, $affiliate2 )
 		) );
 
 		$affiliates = wp_list_pluck( $payouts, 'affiliate_id' );
 
 		$this->assertTrue(
-			in_array( $this->_affiliate_id, $affiliates, true )
-			&& in_array( $this->_affiliate_id2, $affiliates, true )
+			in_array( $affiliate1, $affiliates, true )
+			&& in_array( $affiliate2, $affiliates, true )
 		);
 	}
 
-	//
-	// Helpers
-	//
-
-	/**
-	 * Helper to set up payouts.
-	 *
-	 * @since 1.9
-	 * @access public
-	 *
-	 * @param int   $count       Optional. Number of payouts to create. Default 3.
-	 * @param array $payout_args Optional. Arguments for adding payouts. Default empty array.
-	 */
-	public function _set_up_payouts( $count = 3, $payout_args = array() ) {
-		$args = array_merge( array(
-			'affiliate_id' => $this->_affiliate_id,
-			'referrals'    => $this->_referrals
-		), $payout_args );
-
-		for ( $i = 0; $i < $count; $i++ ) {
-			$this->_payouts[] = affiliate_wp()->affiliates->payouts->add( $args );
-		}
-	}
 }
